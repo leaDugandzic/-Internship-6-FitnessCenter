3ba3542404b44507c05550002d0d34eb1c606d73
leaDugandzic <lea.dugandzic07@gmail.com>
2024-12-15 16:51:07 +0100

    adding tables Activity, ActivityTrainer, Memnber, Participation

--- fitnessCentar.sql
+++ fitnessCentar.sql
@@ -25,3 +25,32 @@ CREATE TABLE ActivityType (
     TypeID SERIAL PRIMARY KEY,
     Name VARCHAR(50) NOT NULL CHECK (Name IN ('Strength Training', 'Cardio', 'Yoga', 'Dance', 'Injury Rehabilitation'))
 );
+
+CREATE TABLE Activity (
+    ActivityID SERIAL PRIMARY KEY,
+    ActivityCode VARCHAR(10) NOT NULL UNIQUE,
+    TypeID INT NOT NULL REFERENCES ActivityType(TypeID),
+    Schedule TIMESTAMP NOT NULL,
+    Capacity INT NOT NULL,
+    PricePerSession NUMERIC(10, 2) NOT NULL
+);
+
+CREATE TABLE ActivityTrainer (
+    ActivityTrainerID SERIAL PRIMARY KEY,
+    ActivityID INT NOT NULL REFERENCES Activity(ActivityID),
+    TrainerID INT NOT NULL REFERENCES Trainer(TrainerID),
+    TrainerRole VARCHAR(20) NOT NULL CHECK (TrainerRole IN ('Head Trainer', 'Assistant Trainer'))
+);
+
+CREATE TABLE Member (
+    MemberID SERIAL PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Participation (
+    ParticipationID SERIAL PRIMARY KEY,
+    MemberID INT NOT NULL REFERENCES Member(MemberID),
+    ActivityID INT NOT NULL REFERENCES Activity(ActivityID),
+    RegistrationDate TIMESTAMP NOT NULL DEFAULT NOW()
+);
