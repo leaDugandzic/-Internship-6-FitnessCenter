138297401339613a2f2b0e28fb95273c50b11153
leaDugandzic <lea.dugandzic07@gmail.com>
2024-12-15 19:49:48 +0100

    Add query to aggregate head trainers for activities

--- queries.sql
+++ queries.sql
@@ -10,3 +10,13 @@ SELECT
     c.AverageSalary
 FROM Trainer t
 JOIN Country c ON t.CountryID = c.CountryID;
+
+SELECT 
+    a.ActivityCode, 
+    a.Schedule, 
+    STRING_AGG(CONCAT(t.LastName, ', ', LEFT(t.FirstName, 1)), '; ') AS HeadTrainers
+FROM Activity a
+JOIN ActivityTrainer at ON a.ActivityID = at.ActivityID
+JOIN Trainer t ON at.TrainerID = t.TrainerID
+WHERE at.TrainerRole = 'Head Trainer'
+GROUP BY a.ActivityCode, a.Schedule;
