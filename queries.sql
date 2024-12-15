19b01a49a7aba1bd42e5b04eef1ebae0760d6492
leaDugandzic <lea.dugandzic07@gmail.com>
2024-12-15 20:06:35 +0100

    Add query to list top 3 fitness centers by activity count

--- queries.sql
+++ queries.sql
@@ -20,3 +20,14 @@ JOIN ActivityTrainer at ON a.ActivityID = at.ActivityID
 JOIN Trainer t ON at.TrainerID = t.TrainerID
 WHERE at.TrainerRole = 'Head Trainer'
 GROUP BY a.ActivityCode, a.Schedule;
+
+SELECT 
+    fc.Name AS FitnessCenter, 
+    COUNT(a.ActivityID) AS ActivityCount
+FROM FitnessCenter fc
+JOIN Trainer t ON fc.FitnessCenterID = t.FitnessCenterID
+JOIN ActivityTrainer at ON t.TrainerID = at.TrainerID
+JOIN Activity a ON at.ActivityID = a.ActivityID
+GROUP BY fc.FitnessCenterID, fc.Name
+ORDER BY ActivityCount DESC
+LIMIT 3;
